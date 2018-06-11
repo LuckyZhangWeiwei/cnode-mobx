1e7e60e2ed72d5ac03a9defd711a561129d89e30
Wei Wei Zhang <WWEZ@NNE.COM>
2018-06-11 13:40:55 +0800

    change

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -1,7 +1,9 @@
 const path = require('path')
 const HTMLPlugin = require('html-webpack-plugin')
 
-module.exports = {
+const isDev = process.env.NODE_ENV === 'development'
+
+const config = {
     entry: {
         app: path.join(__dirname,'../client/app.js')
     },
@@ -31,3 +33,21 @@ module.exports = {
         })
     ]
 } 
+
+if(isDev) {
+    config.devServer = {
+        host: '0.0.0.0',
+        port: '8888',
+        contentBase: path.join(__dirname,'../dist'),
+        // hot: true,
+        overlay: {
+            errors: true
+        },
+        publicPath: '/public/',
+        historyApiFallback: {
+            index: '/public/index.html'
+        }
+    }
+}
+
+module.exports = config
