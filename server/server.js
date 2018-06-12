9b40972f6480d20bafd7ef591c6bbb9662be8516
Wei Wei Zhang <WWEZ@NNE.COM>
2018-06-12 10:52:00 +0800

    change

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const favicon = require('serve-favicon')
 const ReactSSR = require('react-dom/server')
 const fs = require('fs')
 const path = require('path')
@@ -8,11 +9,13 @@ const isDev = process.env.NODE_ENV==='development'
 
 const app = express()
 
+app.use(favicon(path.join(__dirname, '../favicon.ico')))
+
 if (!isDev) {
     const serverEntry = require('../dist/server.entry').default
 
     const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8')
-    
+
     app.use('/public/', express.static(path.join(__dirname, '../dist')))
 
     app.get('*', (req, res) => {
@@ -21,7 +24,7 @@ if (!isDev) {
      })
 } else {
     const devStatic = require('./utils/dev.static')
-    
+
     devStatic(app)
 }
 
