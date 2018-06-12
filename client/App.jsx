f21c2ca3e384126bbf60b1f5c06b266bf6b59ba9
Wei Wei Zhang <WWEZ@NNE.COM>
2018-06-12 10:07:13 +0800

    change

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  componentDidMount() {
+    // do
+  }
   render() {
-      return (
-          <div>this is app.</div>
-      )
+    return (
+      <div>this is app.</div>
+    )
   }
 }
--- client/app.js
+++ client/app.js
@@ -1,26 +1,24 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
+import { AppContainer } from 'react-hot-loader' //eslint-disable-line
 import App from './App.jsx'
-
-
 
 const root = document.getElementById('root')
 const render = (Component) => {
-    const renderMethod=module.hot ? ReactDom.render :ReactDom.hydrate
-    renderMethod(
-        <AppContainer>
-          <Component />
-        </AppContainer>,
-        root
-    )
+  const renderMethod = module.hot ? ReactDom.render : ReactDom.hydrate
+  renderMethod(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    root,
+  )
 }
 render(App)
 
 if (module.hot) {
-    module.hot.accept('./App.jsx', () => {
-      const NextApp = require('./App.jsx').default
-      render(NextApp)
-    })
+  module.hot.accept('./App.jsx', () => {
+    const NextApp = require('./App.jsx').default //eslint-disable-line
+    render(NextApp)
+  })
 }
 
