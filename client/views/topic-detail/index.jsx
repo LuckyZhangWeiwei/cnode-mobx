f4bd657db9af81177c3328bdc968c5827226017e
Wei Wei Zhang <WWEZ@NNE.COM>
2018-06-12 11:21:37 +0800

    change

---
+++ client/views/topic-detail/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class TopicDetail extends React.Component {
+  componentDidMount() {
+    // do
+  }
+
+  render() {
+    return (
+      <div>this is a topic Detail</div>
+    )
+  }
+}
---
+++ client/views/topic-list/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class TopicList extends React.Component {
+  componentDidMount() {
+    // do
+  }
+
+  render() {
+    return (
+      <div>this is a topic list</div>
+    )
+  }
+}
