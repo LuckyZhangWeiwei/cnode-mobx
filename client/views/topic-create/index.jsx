68caeb98ccae1a695eb764714e6899ca88284750
Wei Wei Zhang <WWEZ@NNE.COM>
2018-06-27 15:21:42 +0800

    change

--- client/views/topic-create/index.jsx
+++ client/views/topic-create/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Container from './../layout/container'
 
-export default class TopicDetail extends React.Component {
+export default class TopicCreate extends React.Component {
   componentDidMount() {
     // do
   }
