d0ed99ed601e5d70a759279d95cab0847020ca66
Wei Wei Zhang <WWEZ@NNE.COM>
2018-07-04 14:18:42 +0800

    change

--- client/views/layout/drawer.jsx
+++ client/views/layout/drawer.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import { inject, observer } from 'mobx-react'
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
 import { tabs } from '../../util/variable-define'
+import UserAvatar from '../layout/user-avatar'
 
 const styles = {
   list: {
@@ -17,12 +19,34 @@ const styles = {
   },
 };
 
+@inject(stores => (
+  {
+    user: stores.appState.user,
+    appState: stores.appState,
+    currentPath: stores.appState.currentPath,
+  }
+))
+@observer
 class DrawerMenu extends React.Component {
   static contextTypes = {
     router: PropTypes.object,
   }
+  constructor(props) {
+    super(props)
+    this.goTo = this.goTo.bind(this)
+  }
   changeTab(value) {
     this.context.router.history.push(`/index/${value}`)
+  }
+
+  goTo() {
+    if (this.props.user.isLogin) {
+      this.context.router.history.push('/user/info')
+      this.props.appState.setCurrentPath('/user/info')
+    } else {
+      this.context.router.history.push('/user/login')
+      this.props.appState.setCurrentPath('/user/login')
+    }
   }
 
   render() {
@@ -31,6 +55,12 @@ class DrawerMenu extends React.Component {
     const sideList = (
       <div className={classes.list}>
         <List>
+          <ListItem>
+            <UserAvatar onClick={this.goTo} />
+          </ListItem>
+        </List>
+        <Divider />
+        <List>
           {
             Object.keys(tabs).map(t => (
               <ListItem button key={t} onClick={() => this.changeTab(t)}>
@@ -38,24 +68,6 @@ class DrawerMenu extends React.Component {
               </ListItem>
           ))
           }
-        </List>
-        <Divider />
-        <List>
-          <ListItem key="item-1">
-            <ListItemText primary="item-1" />
-          </ListItem>
-          <ListItem key="item-2">
-            <ListItemText primary="item-2" />
-          </ListItem>
-          <ListItem key="item-3">
-            <ListItemText primary="item-3" />
-          </ListItem>
-          <ListItem key="item-4">
-            <ListItemText primary="item-4" />
-          </ListItem>
-          <ListItem key="item-5">
-            <ListItemText primary="item-5" />
-          </ListItem>
         </List>
       </div>
     );
@@ -81,6 +93,8 @@ DrawerMenu.propTypes = {
   classes: PropTypes.object.isRequired,
   isOpen: PropTypes.bool.isRequired,
   onClose: PropTypes.bool.isRequired,
+  user: PropTypes.bool.isRequired,
+  appState: PropTypes.bool.isRequired,
 };
 
 export default withStyles(styles)(DrawerMenu);
